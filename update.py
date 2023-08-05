f53caf9c15d3302c70fbd0e25f7952506d6f3aba
TheGreatMaximus98 <87099103+TheGreatMaximus98@users.noreply.github.com>
2023-08-05 18:02:31 -0400

    Update and rename update.py to main.py

--- update.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-update = str(requests.get("https://raw.githubusercontent.com/TheGreatMaximus98/PythonGame/main/main.py").content).replace("b'", "").replace("'", "").replace("\\n\\n", "\n").removesuffix("\\n")
-with open("main.py", "w") as f:
-    f.write(update)
